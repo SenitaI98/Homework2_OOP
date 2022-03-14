eb9bf32406cb9e1d64f41a439c98651ab66624ea
SenitaI98 <senita.isovic@live.com>
2022-03-14 10:10:23 +0100

    Task3

---
+++ Main3.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class Main3 {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(1, 1, true);
+        coffeeMachine.chooseCoffee();
+        coffeeMachine.priceToPay();
+    }
+}
