489781897bae61d763944589d07841a6bf4370a1
SenitaI98 <senita.isovic@live.com>
2022-03-21 23:22:26 +0100

    updated-task1

--- Main1.java
+++ Main1.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Main1 {
     public static void main(String[] args) {
         Numbers numbers = new Numbers( new ArrayList<>());
-        numbers.whileLoopAddNumbers();
-        System.out.println("\nMin value in ArrayList is: " + numbers.listOfNumbers.get(numbers.minValue()));
-        System.out.println("Max value in ArrayList is: " + numbers.listOfNumbers.get(numbers.maxValue()));
+        numbers.addNumbersBasedOnUserInput();
+        numbers.minValue();
+        numbers.maxValue();
         numbers.sumOfNumbersInArrayList();
         numbers.isNumberPrime();
 
