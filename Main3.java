a1322b308c0b3a6ae53cf2afd85d05606637e281
SenitaI98 <senita.isovic@live.com>
2022-03-24 09:59:38 +0100

    update-throw exepction

--- Main3.java
+++ Main3.java
@@ -1,8 +1,8 @@
 package task3;
 
 public class Main3 {
-    public static void main(String[] args) {
-        CoffeeMachine coffeeMachine = new CoffeeMachine(1, 1, true);
+    public static void main(String[] args) throws Exception {
+        CoffeeMachine coffeeMachine = new CoffeeMachine(1, 1, false);
         coffeeMachine.chooseCoffee();
         coffeeMachine.priceToPay();
     }
